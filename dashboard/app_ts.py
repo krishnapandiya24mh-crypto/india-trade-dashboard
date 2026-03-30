f5431aeda462696db6565394b706f6bf76899e25
Krishna Pandiya <krishna.pandiya24mh@ibsindia.org>
2026-03-30 19:20:55 +0530

    move db_cloud to root

--- dashboard/app_ts.py
+++ dashboard/app_ts.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.db_cloud import get_engine, q, get_stats, is_cloud
+from db_cloud import get_engine, q, get_stats, is_cloud
 engine = get_engine()
 
 """
